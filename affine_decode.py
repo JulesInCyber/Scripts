32239929067d59fc71ebff4cecebd34dceb9f99a
Julian Jung <tick.73spatial@icloud.com>
2025-06-30 12:41:09 +0200

    Update affine_decode.py
    - Temporarily removed CLI-Features
    - Added Decoding

--- affine_decode.py
+++ affine_decode.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-message = sys.argv[1]
+# message = sys.argv[1]
 
 a_tup = ((1,1),(3,9),(5,21),(7,15),(9,3),(11,19),(15,7),(17,23),(19,11),(21,5),(23,17),(25,25))
 
@@ -21,4 +21,22 @@ def encode_affine(cleartext):
     print(f"\t[!] Encryption Key: ({a},{b})\n\n\t[!] Encoded Message: {cipher_text}")
     return cipher_text 
 
-encode_affine(message)
+def decode_affine(key_a, key_b, ciphertext):
+    for pair in a_tup:
+        if pair[0] == key_a:
+            a_inv = pair[1]
+            break
+    clear = []
+    for letter in ciphertext.upper():
+        if ord(letter) in range(65,91):
+            y = ord(letter) - ord("A")
+            c = a_inv * (y - key_b) % 26
+            c_letter = chr(c + ord("A"))
+            clear.append(c_letter)
+        else:
+            clear.append(letter)
+    clear_text = "".join(clear)
+    print(clear_text)
+    return clear_text
+
+print(decode_affine(3,10,"Fkrra Ywrp"))
