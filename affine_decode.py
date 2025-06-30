0c635fee94bb3cb667212ffb7a6deea2e67327bd
Julian Jung <tick.73spatial@icloud.com>
2025-06-30 12:28:14 +0200

    encoding via Commandline

--- affine_decode.py
+++ affine_decode.py
@@ -1,4 +1,7 @@
 import random
+import sys
+
+message = sys.argv[1]
 
 a_tup = ((1,1),(3,9),(5,21),(7,15),(9,3),(11,19),(15,7),(17,23),(19,11),(21,5),(23,17),(25,25))
 
@@ -10,12 +13,12 @@ def encode_affine(cleartext):
         if ord(letter) in range(65,91):
             x = ord(letter) - ord("A")
             c = (a * x + b) % 26
-            print(c)
             c_letter= chr(c + ord("A"))
             cipher.append(c_letter)
         else:
             cipher.append(letter)
     cipher_text =  "".join(cipher)
-    print(f"Ciphertext: {cipher_text}\nKey: ({a},{b})")
+    print(f"\t[!] Encryption Key: ({a},{b})\n\n\t[!] Encoded Message: {cipher_text}")
+    return cipher_text 
 
-encode_affine("Hello World!")
+encode_affine(message)
